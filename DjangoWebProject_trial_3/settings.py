bd3566dd14cb7799e1d4c11d2f8e1fbab6fa08b4
Bart Kung <bartkung@gmail.com>
2020-08-16 00:51:48 -0700

    set up templates

--- DjangoWebProject_trial_3/settings.py
+++ DjangoWebProject_trial_3/settings.py
@@ -30,7 +30,8 @@ ALLOWED_HOSTS = []
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
 INSTALLED_APPS = [
-    'app',
+    'app','HelloDjangoApp',
+      
     # Add your apps here to enable them
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 # Middleware framework
